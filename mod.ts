0198c6d6f1140ee9d8928d9d8896c38d406c3a62
erickmerchant <erickmerchant@gmail.com>
2026-07-29 14:27:48 -0700

    Rewrite to be a plugin

--- mod.ts
+++ mod.ts
@@ -1,96 +1,133 @@
-import type { FlintCacheItem, FlintRouteHandler } from "@flint/framework";
+import type { FlintApplication, FlintRouteContext } from "@flint/framework";
 import { glob, pattern as p } from "@flint/framework";
 import sharp from "sharp";
 import * as Path from "@std/path";
 import * as Fs from "@std/fs";
 
-export type FlintImageMeta = {
+export type FlintImageMeta = Record<string, {
   height: number;
   width: number;
-  name: string;
-};
+  src: string;
+}>;
 
 export type FlintImageSizes = Record<
   string,
   { height: number } | { width: number }
 >;
 
-export async function list(
-  pathname: string,
-): Promise<Array<FlintImageMeta>> {
-  const ext = Path.extname(pathname);
-  const imagePromises = [];
-  let i = 0;
-
-  for await (const { name, path } of Fs.expandGlob(pathname)) {
-    imagePromises.push(
-      sharp(path)
-        .metadata()
-        .then((metadata) => ({
-          height: metadata.height,
-          width: metadata.width,
-          name: Path.basename(name, ext),
-          index: i++,
-        })),
-    );
-  }
-
-  let images: Array<FlintImageMeta> = await Promise.all(imagePromises);
-  const orders = new Uint32Array(images.length);
-
-  globalThis.crypto.getRandomValues(orders);
-
-  images = Object.keys(images).sort((a, b) => orders[+a] - orders[+b]).map((
-    k,
-  ) => images[+k]);
-
-  return images;
-}
-
 export default function (
-  pattern: URLPattern,
+  directory: string,
   sizes: FlintImageSizes,
-): [string | URLPattern, FlintRouteHandler, FlintCacheItem] {
+  type?: "jpeg" | "webp" | "avif",
+): ((app: FlintApplication) => void) & {
+  list: () => Promise<Array<FlintImageMeta>>;
+} {
+  const pattern = p`/${directory}/:name*.jpeg`;
   const path = pattern.pathname;
   const ext = Path.extname(path);
   const basename = Path.basename(path, ext);
   const dirname = Path.dirname(path);
 
-  return [
-    p`${dirname === "/" ? "" : dirname}/${basename}/:size${ext}`,
-    ({ params }) => {
-      if (
-        params.name && params.size &&
-        sizes[params.size as keyof typeof sizes] != null
-      ) {
-        const settings = sizes[params.size as keyof typeof sizes];
+  const cb = (app: FlintApplication) => {
+    app.route(
+      p`${dirname === "/" ? "" : dirname}/${basename}/:size.${
+        type ?? ext.slice(1)
+      }`,
+      ({ params }: FlintRouteContext) => {
+        if (
+          params.name && params.size &&
+          sizes[params.size as keyof typeof sizes] != null
+        ) {
+          const settings = sizes[params.size as keyof typeof sizes];
 
-        let img = sharp(`media/images/${params.name}.jpeg`);
+          let img = sharp(`./${directory}/${params.name}${ext}`);
 
-        if ("height" in settings || "width" in settings) {
-          img = img.resize(settings);
+          if ("height" in settings || "width" in settings) {
+            img = img.resize(settings);
+          }
+
+          const method = type ?? ext.slice(1);
+
+          if (method === "jpeg" || method === "webp" || method === "avif") {
+            img = img[method]({
+              quality: 100,
+              progressive: true,
+              force: true,
+            });
+          }
+
+          return img.toBuffer() as Promise<Uint8Array<ArrayBuffer>>;
         }
 
-        img = img.jpeg({
-          quality: 100,
-          progressive: true,
-          force: true,
-        });
+        throw Error("not found");
+      },
+      glob(pattern, (
+        _,
+        { name },
+      ) =>
+        Object
+          .keys(sizes)
+          .map((size) => [
+            `/${directory}/${name}/${size}.${type ?? ext.slice(1)}`,
+          ])
+          .flat()),
+    );
+  };
 
-        return img.toBuffer() as Promise<Uint8Array<ArrayBuffer>>;
-      }
+  cb.list = async (): Promise<Array<FlintImageMeta>> => {
+    const pattern = `./${directory}/**/*.jpeg`;
+    const ext = Path.extname(pattern);
+    const imagePromises = [];
 
-      throw Error("not found");
-    },
-    glob(pattern, (
-      _,
-      { name },
-    ) =>
-      Object
-        .keys(sizes)
-        .map((size) => [
-          `/media/images/${name}/${size}.jpeg`,
-        ])
-        .flat()),
-  ];
+    for await (let { name, path } of Fs.expandGlob(pattern)) {
+      name = Path.basename(name, ext);
+
+      imagePromises.push(
+        sharp(path)
+          .metadata()
+          .then((metadata) => {
+            const result: FlintImageMeta = {};
+
+            for (const key of Object.keys(sizes)) {
+              const config = sizes[key];
+              let height = metadata.height;
+              let width = metadata.width;
+
+              if ("height" in config) {
+                height = config.height;
+                width = metadata.width / metadata.height *
+                  config.height;
+              }
+
+              if ("width" in config) {
+                width = config.width;
+                height = metadata.height / metadata.width *
+                  config.width;
+              }
+
+              result[key] = {
+                src: `/${directory}/${name}/${key}.${type}`,
+                height,
+                width,
+              };
+            }
+
+            return result;
+          }),
+      );
+    }
+
+    let images: Array<FlintImageMeta> = await Promise.all(imagePromises);
+    const orders = new Uint32Array(images.length);
+
+    globalThis.crypto.getRandomValues(orders);
+
+    images = Object.keys(images).sort((a, b) => orders[+a] - orders[+b]).map((
+      k,
+    ) => images[+k]);
+
+    return images;
+  };
+
+  return cb;
 }
